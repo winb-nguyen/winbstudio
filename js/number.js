cc50841455dd1f4bfb80a9878e89030e6cdb9379
winb-nguyen <75201455+winb-nguyen@users.noreply.github.com>
2021-07-06 20:58:13 +0700

    Add files via upload

---
+++ js/number.js
@@ -0,0 +1,36 @@
+$.fn.jQuerySimpleCounter = function( options ) {
+	    var settings = $.extend({
+	        start:  0,
+	        end:    100,
+	        easing: 'swing',
+	        duration: 400,
+	        complete: ''
+	    }, options );
+
+	    var thisElement = $(this);
+
+	    $({count: settings.start}).animate({count: settings.end}, {
+			duration: settings.duration,
+			easing: settings.easing,
+			step: function() {
+				var mathCount = Math.ceil(this.count);
+				thisElement.text(mathCount);
+			},
+			complete: settings.complete
+		});
+	};
+
+
+$('#number1').jQuerySimpleCounter({end: 1200,duration: 3000});
+$('#number2').jQuerySimpleCounter({end: 555,duration: 3000});
+$('#number3').jQuerySimpleCounter({end: 35,duration: 3000});
+$('#number4').jQuerySimpleCounter({end: 2246,duration: 3000});
+
+
+
+  	/* AUTHOR LINK */
+     $('.about-me-img').hover(function(){
+            $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+        }, function(){
+            $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+        });
